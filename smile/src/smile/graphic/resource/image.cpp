8e7d976a8400fee73bf4505693b3624c44a1a31b
ZennGeeraerts <76271773+ZennGeeraerts@users.noreply.github.com>
2025-03-29 20:41:42 +0100

    feat: Add stb image, read png images

---
+++ smile/src/smile/graphic/resource/image.cpp
@@ -0,0 +1,60 @@
+/*=============================================================================*/
+// Copyright 2022-2025 Smile Engine
+// Authors: Zenn Geeraerts
+/*=============================================================================*/
+#include "smpch.h"
+#include "image.h"
+
+namespace smile::graphic
+{
+    Image::Image( const Uint32 width, const Uint32 height, const ImageFormat format, const Byte *pData )
+        : m_Width{ width }, m_Height{ height }, m_Format{ format }
+    {
+        switch ( format )
+        {
+            case ImageFormat::RGBA:
+            case ImageFormat::BGRA:
+            {
+                m_Stride = 4;
+            }
+            break;
+
+            case ImageFormat::RGB:
+            {
+                m_Stride = 3;
+            }
+            break;
+
+            case ImageFormat::BC1:
+            case ImageFormat::BC4:
+            {
+                m_Stride = 0;
+            }
+            break;
+
+            case ImageFormat::RG32F:
+            {
+                m_Stride = 8;
+            }
+            break;
+
+            default:
+            {
+                m_Stride = 0;
+                SM_ASSERT( false, "Image > Unsupported format" )
+            }
+            break;
+        }
+
+        const Uint32 size = [&]()
+        {
+            if ( m_Stride == 0 )
+                return m_Width * m_Height / 2;
+            else
+                return m_Stride * m_Width * m_Height;
+        }();
+
+        m_Buffer.resize( size );
+        std::memcpy( m_Buffer.data(), pData, size );
+    }
+}
---
+++ smile/src/smile/graphic/resource/image.h
@@ -0,0 +1,61 @@
+/*=============================================================================*/
+// Copyright 2022-2025 Smile Engine
+// Authors: Zenn Geeraerts
+/*=============================================================================*/
+#pragma once
+
+#include "smile/common/memory/object.h"
+
+namespace smile::graphic
+{
+    enum class ImageFormat : Uint8
+    {
+        RGB,
+        RGBA,
+        BGRA,
+        BC1,
+        BC2,
+        BC3,
+        BC4,
+        BC5,
+        BC6,
+        BC7,
+        RG32F,
+        Count,
+        None
+    };
+
+    class Image final : public memory::Object
+    {
+      public:
+        Image() = default;
+        Image( const Uint32 width, const Uint32 height, const ImageFormat format, const Byte *pData );
+
+        Uint32 GetWidth() const
+        {
+            return m_Width;
+        }
+
+        Uint32 GetHeight() const
+        {
+            return m_Height;
+        }
+
+        ImageFormat GetFormat() const
+        {
+            return m_Format;
+        }
+
+        const Byte *GetData() const
+        {
+            return m_Buffer.data();
+        }
+
+      private:
+        Uint32 m_Width;
+        Uint32 m_Height;
+        ImageFormat m_Format;
+        std::vector< Byte > m_Buffer;
+        Uint32 m_Stride;
+    };
+}
