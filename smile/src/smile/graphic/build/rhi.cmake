6ed7d4fb4556ee1fbf728f89f45a69529cd0e68d
Zenn Geeraerts <geeraertszenn@gmail.com>
2025-06-28 16:06:36 +0200

    fix: Only build rhi

---
+++ smile/src/smile/graphic/build/rhi.cmake
@@ -0,0 +1,6 @@
+smile_option(RENDERER "Include rendering hardware interface" ON)
+
+if(SMILE_RHI)
+	get_files_recursively(SOURCE_FILES "rhi")
+	target_sources(smile PRIVATE ${SOURCE_FILES})
+endif()
