06d2fef40964efe83d373e92071a66cb5de3516c
Zenn Geeraerts <geeraertszenn@gmail.com>
2025-09-08 23:25:00 +0200

    fix: Revert changes in framebuffer from merge

--- smile/src/platform/directx11/graphic/rhi/resource/directx11_frame_buffer.cpp
+++ smile/src/platform/directx11/graphic/rhi/resource/directx11_frame_buffer.cpp
@@ -1,59 +1,31 @@
 /*=============================================================================*/
-// Copyright 2022-2023 Smile Engine
+// Copyright 2022-2025 Smile Engine
 // Authors: Zenn Geeraerts
 /*=============================================================================*/
-
 #include "smpch.h"
 #include "directx11_frame_buffer.h"
 
-#include "platform/directx11/graphic/rhi/directx11_diagnostics.h"
+#include "platform/directx11/graphic/rhi/directx11_device.h"
 
-namespace smile::graphic
+#include <d3d11.h>
+
+namespace smile::graphic::rhi
 {
-    static bool IsDepthFormat( FramebufferTextureFormat format )
-    {
-        switch ( format )
-        {
-            case FramebufferTextureFormat::Depth24Stencil8:
-                return true;
-
-            default:
-                return false;
-        }
-    }
-
-    static DXGI_FORMAT FramebufferTextureFormatToDirectXBaseType( FramebufferTextureFormat format )
-    {
-        switch ( format )
-        {
-            case FramebufferTextureFormat::RGBA8:
-                return DXGI_FORMAT_B8G8R8A8_UNORM;
-                break;
-            case FramebufferTextureFormat::Depth24Stencil8:
-                return DXGI_FORMAT_D24_UNORM_S8_UINT;
-                break;
-
-            case FramebufferTextureFormat::None:
-            default:
-                SM_ASSERT_MSG(
-                    false, "DirectX11Framebuffer::framebufferTextureFormatToDirectXBaseType > Unknown ShaderDataType" );
-                return DXGI_FORMAT_UNKNOWN;
-        }
-    }
-
-    void DirectX11Framebuffer::Create( ID3D11Device *pDevice, const FramebufferDescriptor &desc )
+    void DirectX11Framebuffer::Create( DirectX11Device &device, const FramebufferDescriptor &desc )
     {
         Descriptor = desc;
+        FramebufferInfo = FramebufferInfoExtented{ desc };
 
-        for ( const auto &framebufferTextureData : desc.Attachments.Attachments )
+        for ( const auto &colorAttachment : desc.ColorAttachments )
         {
-            if ( !IsDepthFormat( framebufferTextureData.TextureFormat ) )
-                ColorAttachmentData.push_back( framebufferTextureData );
-            else
-                DepthAttachmentData = framebufferTextureData;
+            SM_ASSERT( colorAttachment.IsValid() );
+            pRenderTargetViews.PushBack( device.GetOrCreateRenderTargetViewForAttachment( colorAttachment ) );
         }
 
-        Invalidate( pDevice );
+        if ( desc.DepthAttachment.IsValid() )
+        {
+            pDepthStencilView = device.GetOrCreateDepthStencilViewForAttachment( desc.DepthAttachment );
+        }
     }
 
     void DirectX11Framebuffer::Destroy()
