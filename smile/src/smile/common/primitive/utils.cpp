45f9ced3106f7341c05aaa164b4f56f961071299
Zenn Geeraerts <geeraertszenn@gmail.com>
2025-11-04 10:29:18 +0100

    feat: Add ToInt and ToFloat util functions + tests

--- smile/src/smile/common/primitive/utils.cpp
+++ smile/src/smile/common/primitive/utils.cpp
@@ -18,4 +18,122 @@ namespace smile::primitive
         std::wstring wideString{ str.begin(), str.end() };
         return wideString;
     }
+
+    std::optional< int > ToInt( StringView text )
+    {
+        if ( text.IsEmpty() )
+        {
+            return std::nullopt;
+        }
+
+        const bool isNegative{ text[0] == '-' };
+        Index textIndex = [&]()
+        {
+            if ( text[0] == '+' || isNegative )
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }();
+
+        const Count charCount = text.GetCharCount();
+        if ( textIndex >= charCount )
+        {
+            return std::nullopt;
+        }
+
+        int result{ 0 };
+
+        while ( textIndex < charCount )
+        {
+            const auto character = text[textIndex];
+            if ( character >= '0' && character <= '9' )
+            {
+                result = result * 10 + ( character - '0' );
+            }
+            else
+            {
+                SM_LOG_ERROR( "Invalid integer '{}' passed to ToInt", text );
+                return std::nullopt;
+            }
+
+            ++textIndex;
+        }
+
+        return isNegative ? -result : result;
+    }
+
+    std::optional< float > ToFloat( StringView text )
+    {
+        if ( text.IsEmpty() )
+        {
+            return std::nullopt;
+        }
+
+        const bool isNegative{ text[0] == '-' };
+        bool isDecimal{ text[0] == '.' };
+
+        Count charactersPastDecimal{ 0 };
+        Index textIndex = [&]()
+        {
+            if ( text[0] == '+' || isNegative )
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }();
+
+        if ( text[textIndex] == '.' )
+        {
+            isDecimal = true;
+            ++charactersPastDecimal;
+            ++textIndex;
+        }
+
+        const Count charCount = text.GetCharCount();
+        if ( textIndex >= charCount )
+        {
+            return std::nullopt;
+        }
+
+        float result{ 0.0f };
+
+        while ( textIndex < charCount )
+        {
+            const auto character = text[textIndex];
+
+            if ( character >= '0' && character <= '9' )
+            {
+                if ( !isDecimal )
+                {
+                    result = result * 10 + ( character - '0' );
+                }
+                else
+                {
+                    result = result + ( character - '0' ) / pow( 10, charactersPastDecimal );
+                    ++charactersPastDecimal;
+                }
+            }
+            else if ( character == '.' )
+            {
+                isDecimal = true;
+                ++charactersPastDecimal;
+            }
+            else
+            {
+                SM_LOG_ERROR( "Invalid float '{}' passed to ToFloat", text );
+                return std::nullopt;
+            }
+
+            ++textIndex;
+        }
+
+        return isNegative ? -result : result;
+    }
 }
--- smile/src/smile/common/primitive/utils.h
+++ smile/src/smile/common/primitive/utils.h
@@ -3,6 +3,9 @@
 // Authors: Zenn Geeraerts
 /*=============================================================================*/
 #pragma once
+
+#include "text/string_view.h"
+
 #include <string>
 
 namespace smile::primitive
@@ -10,4 +13,7 @@ namespace smile::primitive
     // TODO: Move to string class
     std::string WideStringToString( const std::wstring &wideString );
     std::wstring StringToWideString( const std::string &str );
+
+    std::optional< int > ToInt( StringView text );
+    std::optional< float > ToFloat( StringView text );
 }
