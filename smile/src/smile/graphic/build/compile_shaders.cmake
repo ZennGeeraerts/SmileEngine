01c97c58a538becc608b803b1d04c5532b579ace
Zenn Geeraerts <geeraertszenn@gmail.com>
2025-11-12 17:11:24 +0100

    fix: Split shader compilation from shader build file

---
+++ smile/src/smile/graphic/build/compile_shaders.cmake
@@ -0,0 +1,37 @@
+smile_option(COMPILE_SHADERS "Use shader compilation" ON)
+
+if(SMILE_COMPILE_SHADERS)
+    set(COMPILED_SHADERS)
+
+    set(SHADER_SOURCE_DIR ${SMILE_PATH}/smile/graphic/shader)
+
+    set(SHADER_OUTPUT_DIR ${CMAKE_BINARY_DIR}/${CMAKE_CFG_INTDIR}/resources/shaders)
+
+    file(MAKE_DIRECTORY ${SHADER_OUTPUT_DIR})
+
+    file(GLOB_RECURSE SHADER_FILES
+        "${SHADER_SOURCE_DIR}/*.hlsl"
+    )
+
+    foreach(SHADER ${SHADER_FILES})
+        get_filename_component(SHADER_FILENAME ${SHADER} NAME)
+        string(REPLACE ".hlsl" ".smshader" SHADER_OUTPUT_FILE ${SHADER_FILENAME})
+        set(SHADER_OUTPUT "${SHADER_OUTPUT_DIR}/${SHADER_OUTPUT_FILE}")
+
+        add_custom_command(
+            OUTPUT ${SHADER_OUTPUT}
+            COMMAND $<TARGET_FILE:x-shader-c> ${SHADER} -o ${SHADER_OUTPUT}
+            DEPENDS ${SHADER} x-shader-c
+            COMMENT "Compiling shader ${SHADER_FILENAME}"
+            VERBATIM
+        )
+
+        list(APPEND COMPILED_SHADERS ${SHADER_OUTPUT})
+    endforeach()
+
+    add_custom_target(compile-shaders ALL
+        DEPENDS ${COMPILED_SHADERS}
+    )
+
+    add_dependencies(smile compile-shaders)
+endif()
