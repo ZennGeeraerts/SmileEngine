8cbe6aa84f7e00384f4472976b23c162049dd608
Zenn Geeraerts <geeraertszenn@gmail.com>
2026-01-29 22:34:52 +0100

    fix: Set material parameters and texture bindings

--- smile/src/smile/graphic/scene/material/material.cpp
+++ smile/src/smile/graphic/scene/material/material.cpp
@@ -5,156 +5,52 @@
 #include "smpch.h"
 #include "material.h"
 
-#include "smile/common/memory/memory.h"
-
 namespace smile::graphic
 {
-    static MaterialParamType ConvertConstantTypeToMaterialParamType( ConstantType constantType )
+    Material::Material( const MaterialDescriptor &desc ) : m_Descriptor{ desc }
     {
-        switch ( constantType )
-        {
-            case ConstantType::Float:
-                return MaterialParamType::Float;
-            case ConstantType::Float2:
-                return MaterialParamType::Float2;
-            case ConstantType::Float3:
-                return MaterialParamType::Float3;
-            case ConstantType::Int:
-                return MaterialParamType::Int;
-            case ConstantType::Bool:
-                return MaterialParamType::Bool;
-            default:
-                SM_ASSERT( false, "Unsupported constant type for material system" );
-        }
-    }
-
-    static bool IsConstantBufferMaterialParamType( MaterialParamType type )
-    {
-        switch ( type )
-        {
-            case MaterialParamType::Float:
-            case MaterialParamType::Float2:
-            case MaterialParamType::Float3:
-            case MaterialParamType::Int:
-            case MaterialParamType::Bool:
-                return true;
-            case MaterialParamType::Texture:
-            case MaterialParamType::Sampler:
-                return false;
-            default:
-                SM_ASSERT( false, "Unsupported material parameter type" );
-        }
-    }
-
-    Material::Material()
-    {
-        /*for ( const auto &binding : pMaterialAsset->GetBindings() )
-        {
-            switch ( binding.Value.Type )
-            {
-                case rhi::ResourceType::Texture_SRV:
-                case rhi::ResourceType::Texture_UAV:
-                {
-                    SM_ASSERT( !m_Params.HasItemAtKey( binding.Key ) );
-
-                    MaterialParam param{ binding.Key, MaterialParamType::Texture, Texture::Ref{} };
-                    m_Params.Insert( binding.Key, std::move( param ) );
-                    break;
-                }
-
-                case rhi::ResourceType::Sampler:
-                {
-                    SM_ASSERT( !m_Params.HasItemAtKey( binding.Key ) );
-
-                    MaterialParam param{ binding.Key, MaterialParamType::Sampler, Sampler::Ref{} };
-                    m_Params.Insert( binding.Key, std::move( param ) );
-                    break;
-                }
-
-                case rhi::ResourceType::ConstantBuffer:
-                {
-                    const auto &constantBufferDesc = pMaterialAsset->GetConstantBufferDesc( binding.Key );
-
-                    m_ConstantBufferData.Insert( binding.Key, ConstantBufferData( constantBufferDesc.GetSize() ) );
-
-                    for ( const auto &item : constantBufferDesc )
-                    {
-                        SM_ASSERT( !m_Params.HasItemAtKey( item.Name ) );
-
-                        MaterialParam param{ item.Name,
-                            ConvertConstantTypeToMaterialParamType( item.Type ),
-                            primitive::Vector< Byte >( item.Size ) };
-
-                        m_Params.Insert( item.Name, std::move( param ) );
-                    }
-                    break;
-                }
-
-                default:
-                    SM_ASSERT_MSG( false, "Unsupported resource type" );
-            }
-        }*/
     }
 
     void Material::Clear()
     {
-        m_Params.Clear();
+        // m_Params.Clear();
     }
 
-    void Material::SetParam( const primitive::StringView name, const MaterialParamValue &data )
+    void Material::SetParameter( const primitive::StringView name, const MaterialParameter::Value &data )
     {
-        if ( !m_Params.HasItemAtKey( name ) )
+        if ( !m_Descriptor.Parameters.HasItemAtKey( name ) )
         {
-            SM_LOG_WARNING( "Material::SetParam > Could not find material parameter with name: {}", name );
+            SM_LOG_WARNING( "Material::SetParameter > Could not find material parameter with name: {}", name );
             return;
         }
 
-        auto &param = m_Params.GetItemAtKey( name );
+        auto &param = m_Descriptor.Parameters.GetItemAtKey( name );
         param.Data = data;
 
-        if ( IsConstantBufferMaterialParamType( param.Type ) )
-        {
-            SetConstantBufferData( m_VertexShaderCB, name, data );
-            SetConstantBufferData( m_PixelShaderCB, name, data );
-        }
-        else
-        {
-            rhi::BindingSetDescriptor desc{};
-            std::find_if( desc.begin(), desc.end(), []( const auto &item ) {} );
-        }
+        m_DirtyFlags.Set( DirtyFlags::Parameter );
     }
 
-    void Material::SetConstantBufferData( ConstantBuffer::Ref constantBuffer,
-        const primitive::StringView name,
-        const MaterialParamValue &data )
+    MaterialParameter::Value Material::GetParameter( const primitive::StringView name ) const
     {
-        const auto &desc = constantBuffer->GetDescriptor();
-        auto it = std::find_if( desc.begin(),
-            desc.end(),
-            [name]( const auto &constantBufferItem ) { return constantBufferItem.Name == name; } );
+        return m_Descriptor.Parameters.GetItemAtKey( name ).Data;
+    }
 
-        if ( it != desc.end() )
+    void Material::SetTextureBinding( const primitive::StringView name, Texture::Ref texture )
+    {
+        if ( !m_Descriptor.TextureBindings.HasItemAtKey( name ) )
         {
-            const auto &cbItem = ( *it );
-            const auto &valueData = std::get< primitive::Vector< Byte > >( data );
-
-            SM_ASSERT( cbItem.GetStride() == valueData.GetItemCount() );
-
-            memory::CopyArrayItems( reinterpret_cast< Byte * >( constantBuffer->GetBuffer() ) + cbItem.Offset,
-                cbItem.GetStride(),
-                valueData.GetData() );
+            SM_LOG_WARNING( "Material::SetTextureBinding > Could not find texture binding with name: {}", name );
+            return;
         }
+
+        auto &textureBinding = m_Descriptor.TextureBindings.GetItemAtKey( name );
+        textureBinding.Texture = texture;
+
+        m_DirtyFlags.Set( DirtyFlags::Texture );
     }
 
-    void Material::SetBindingSetData( BindingSet::Ref bindingSet,
-        const primitive::StringView name,
-        const MaterialParamValue &data )
+    Texture::Ref Material::GetTextureBinding( const primitive::StringView name ) const
     {
-        
-    }
-
-    const MaterialParam &Material::GetParam( const primitive::StringView name ) const
-    {
-        return m_Params.GetItemAtKey( name );
+        return m_Descriptor.TextureBindings.GetItemAtKey( name ).Texture;
     }
 }
--- smile/src/smile/graphic/scene/material/material.h
+++ smile/src/smile/graphic/scene/material/material.h
@@ -6,12 +6,11 @@
 
 #include "smile/graphic/renderer/shader/vertex_shader.h"
 #include "smile/graphic/renderer/shader/pixel_shader.h"
-#include "smile/graphic/renderer/shader/constant_buffer.h"
-#include "smile/graphic/renderer/shader/binding_set.h"
 #include "smile/graphic/renderer/resource/texture.h"
-#include "smile/graphic/renderer/resource/sampler.h"
 #include "smile/common/memory/ref.h"
+#include "smile/common/primitive/handle.h"
 #include "smile/common/primitive/collection/hash_map.h"
+#include "smile/common/foundation/flags.h"
 
 #include <DirectXMath.h>
 
@@ -19,36 +18,53 @@
 
 namespace smile::graphic
 {
-    enum class MaterialParamType
+    struct MaterialParameter final
     {
-        Texture,
-        Sampler,
-        Float,
-        Int,
-        Bool,
-        Float2,
-        Float3,
-    };
+        using Value = std::variant< bool, int, float, DirectX::XMFLOAT2, DirectX::XMFLOAT3, Texture::Ref >;
 
-    using MaterialParamValue = std::variant< bool,
-        int,
-        float,
-        DirectX::XMFLOAT2,
-        DirectX::XMFLOAT3,
-        Texture::Ref,
-        rhi::SamplerDescriptor,
-        primitive::Vector< Byte > >;
+        enum class Type
+        {
+            Texture,
+            Sampler,
+            Float,
+            Int,
+            Bool,
+            Float2,
+            Float3,
+        };
 
-    struct MaterialParam final
-    {
-        MaterialParam( const primitive::String &name, MaterialParamType type, const MaterialParamValue &value )
-            : Name{ name }, Type{ type }, Data{ value }
+        MaterialParameter( const primitive::String &name, Type type, const Value &value )
+            : Name{ name }, ParamType{ type }, Data{ value }
         {
         }
 
         primitive::String Name;
-        MaterialParamType Type;
-        MaterialParamValue Data;
+        Type ParamType;
+        Value Data;
+
+        inline bool operator==( const MaterialParameter &other ) const
+        {
+            return Name == other.Name && ParamType == other.ParamType && Data == other.Data;
+        }
+    };
+
+    struct TextureBinding final
+    {
+        primitive::String Name;
+        Texture::Ref Texture;
+
+        inline bool operator==( const TextureBinding &other )
+        {
+            return Name == other.Name && Texture == other.Texture;
+        }
+    };
+
+    struct MaterialDescriptor final
+    {
+        VertexShader::Ref VertexShader;
+        PixelShader::Ref PixelShader;
+        primitive::HashMap< primitive::String, MaterialParameter > Parameters;
+        primitive::HashMap< primitive::String, TextureBinding > TextureBindings;
     };
 
     class Material final : public memory::Counted
@@ -57,42 +73,39 @@ namespace smile::graphic
         using Ref = memory::Ref< Material >;
         using ConstRef = memory::Ref< const Material >;
 
-        Material();
+        using ID = primitive::Handle< Uint32, 24, 8 >;
+
+        Material( const MaterialDescriptor &desc );
+        ~Material() = default;
 
         void Clear();
 
-        inline VertexShader::ConstRef GetVertexShader() const
+        VertexShader::ConstRef GetVertexShader() const
         {
-            return m_pVertexShader;
+            return m_Descriptor.VertexShader;
         }
 
-        inline PixelShader::ConstRef GetPixelShader() const
+        PixelShader::ConstRef GetPixelShader() const
         {
-            return m_pPixelShader;
+            return m_Descriptor.PixelShader;
         }
 
-        void SetParam( const primitive::StringView name, const MaterialParamValue &data );
-        const MaterialParam &GetParam( const primitive::StringView name ) const;
+        void SetParameter( const primitive::StringView name, const MaterialParameter::Value &value );
+        MaterialParameter::Value GetParameter( const primitive::StringView name ) const;
+
+        void SetTextureBinding( const primitive::StringView name, Texture::Ref texture );
+        Texture::Ref GetTextureBinding( const primitive::StringView name ) const;
 
       private:
-        void SetConstantBufferData( ConstantBuffer::Ref constantBuffer,
-            const primitive::StringView name,
-            const MaterialParamValue &data );
+        enum class DirtyFlags
+        {
+            Parameter,
+            Texture,
+        };
 
-        void SetBindingSetData( BindingSet::Ref bindingSet,
-            const primitive::StringView name,
-            const MaterialParamValue &data );
+        MaterialDescriptor m_Descriptor;
+        ID m_ID;
 
-      private:
-        VertexShader::ConstRef m_pVertexShader;
-        PixelShader::ConstRef m_pPixelShader;
-
-        primitive::HashMap< primitive::String, MaterialParam > m_Params;
-
-        ConstantBuffer::Ref m_VertexShaderCB;
-        ConstantBuffer::Ref m_PixelShaderCB;
-
-        BindingSet::Ref m_VertexShaderBindings;
-        BindingSet::Ref m_PixelShaderBindings;
+        foundation::Flags< DirtyFlags > m_DirtyFlags;
     };
 }
